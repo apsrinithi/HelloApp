19929973ecd46141790f22fd732cbdff360bd038
apsrinithi <apsrinithi@gmail.com>
2026-03-18 13:31:03 +0530

    UC4 Displays with multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,17 @@
-public class HelloApp{
-	public static void main (String[] args){
-		    String name= "World";
-			
-			if (args.length > 0)
-            { name = args[0];
-		    }
-			System.out.println("Hello, " + name + "!");
-			
-}
+public class HelloApp {
+    public static void main(String[] args) {
+        String name = "World";
+
+        if (args.length > 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                if (i > 0) 
+					sb.append(", ");
+                    sb.append(args[i]);
+            }
+            name = sb.toString();
+        }
+
+        System.out.println("Hello, " + name);
+    }
 }
